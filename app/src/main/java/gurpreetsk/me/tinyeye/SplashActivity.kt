d8a3dbcd220b11fbd7159dedbb842c3e70695220
Gurpreet Singh <gurpreetsk@hotmail.com>
2018-04-08 01:24:37 +0530

    activity_splash, use ConstraintLayout.

--- app/src/main/java/gurpreetsk/me/tinyeye/SplashActivity.kt
+++ app/src/main/java/gurpreetsk/me/tinyeye/SplashActivity.kt
@@ -6,7 +6,8 @@ import android.support.v7.app.AppCompatActivity
 import com.jakewharton.rxbinding2.view.clicks
 import gurpreetsk.me.tinyeye.cloudvision.activities.MainActivity
 import gurpreetsk.me.tinyeye.ocr.OcrCaptureActivity
-import kotlinx.android.synthetic.main.activity_splash.*
+import kotlinx.android.synthetic.main.activity_splash.recogniseOcrButton
+import kotlinx.android.synthetic.main.activity_splash.recogniseImageButton
 
 class SplashActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,7 +18,7 @@ class SplashActivity : AppCompatActivity() {
   }
 
   private fun bind() {
-    recogniseImageButton.clicks()
+    recogniseOcrButton.clicks()
         .doOnNext { startActivity(Intent(this@SplashActivity, OcrCaptureActivity::class.java)) }
     recogniseImageButton.clicks()
         .doOnNext { startActivity(Intent(this@SplashActivity, MainActivity::class.java)) }
