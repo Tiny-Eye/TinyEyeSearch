3f9c957eb77524017e574bbb8638ee378be81800
Gurpreet Singh <gurpreetsk@hotmail.com>
2018-04-08 02:46:44 +0530

    SplashActivity, extend TinyActivity.

--- app/src/main/kotlin/gurpreetsk/me/tinyeye/SplashActivity.kt
+++ app/src/main/kotlin/gurpreetsk/me/tinyeye/SplashActivity.kt
@@ -4,20 +4,17 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.jakewharton.rxbinding2.view.clicks
+import gurpreetsk.me.tinyeye.base.TinyActivity
 import gurpreetsk.me.tinyeye.cloudvision.activities.MainActivity
 import gurpreetsk.me.tinyeye.ocr.OcrCaptureActivity
 import kotlinx.android.synthetic.main.activity_splash.recogniseOcrButton
 import kotlinx.android.synthetic.main.activity_splash.recogniseImageButton
 
-class SplashActivity : AppCompatActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_splash)
+class SplashActivity : TinyActivity() {
+  override fun inflateLayout(): Int =
+      R.layout.activity_splash
 
-    bind()
-  }
-
-  private fun bind() {
+  override fun bind() {
     recogniseOcrButton.clicks()
         .doOnNext { startActivity(Intent(this@SplashActivity, OcrCaptureActivity::class.java)) }
     recogniseImageButton.clicks()
---
+++ app/src/main/kotlin/gurpreetsk/me/tinyeye/base/TinyActivity.kt
@@ -0,0 +1,17 @@
+package gurpreetsk.me.tinyeye.base
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+abstract class TinyActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(inflateLayout())
+
+    bind()
+  }
+
+  abstract fun bind()
+
+  abstract fun inflateLayout(): Int
+}
