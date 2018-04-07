6f8b330d0648adce99d0021365aea49f9e9683d9
Gurpreet Singh <gurpreetsk@hotmail.com>
2018-04-08 00:48:42 +0530

    SplashActivity, refactor to kotlin, remove unwanted permission logic.

---
+++ app/src/main/java/gurpreetsk/me/tinyeye/SplashActivity.kt
@@ -0,0 +1,25 @@
+package gurpreetsk.me.tinyeye
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.jakewharton.rxbinding2.view.clicks
+import gurpreetsk.me.tinyeye.cloudvision.activities.MainActivity
+import gurpreetsk.me.tinyeye.ocr.OcrCaptureActivity
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_splash)
+
+    bind()
+  }
+
+  private fun bind() {
+    recogniseImageButton.clicks()
+        .doOnNext { startActivity(Intent(this@SplashActivity, OcrCaptureActivity::class.java)) }
+    recogniseImageButton.clicks()
+        .doOnNext { startActivity(Intent(this@SplashActivity, MainActivity::class.java)) }
+  }
+}
