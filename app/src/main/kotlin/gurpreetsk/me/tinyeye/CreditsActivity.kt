4c64922af592316d219802469f03222dde956e74
Gurpreet Singh <gurpreetsk@hotmail.com>
2018-04-22 19:50:22 +0530

    CreditsActivity, convert to Kotlin, update xml.

---
+++ app/src/main/kotlin/gurpreetsk/me/tinyeye/CreditsActivity.kt
@@ -0,0 +1,11 @@
+package gurpreetsk.me.tinyeye
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CreditsActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_credits)
+  }
+}
